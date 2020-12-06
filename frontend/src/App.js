cbf97c44fe9e686f095a3fdc6c6a805ff20dea93
Akash52 <ac8572611@gmail.com>
2020-12-06 09:55:54 +0530

    React Setup

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './bootstrap.min.css'
+import Footer from './Component/Footer'
+import Header from './Component/Header'
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <p>Content !</p>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
