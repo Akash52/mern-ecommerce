c412d7f55afe117f11b2ec0bfb6a0d0d937f4eb7
Akash52 <ac8572611@gmail.com>
2021-06-06 14:54:35 +0530

    Generate a JSON Web Token

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,4 +1,5 @@
 import AsyncHandler from 'express-async-handler'
+import generateToken from '../utils/generateToken.js'
 import User from '../models/userModel.js'
 
 //@desc     Auth user & get Token
@@ -16,7 +17,7 @@ const authUser = AsyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null,
+      token: generateToken(user._id),
     })
   } else {
     res.status(401)
