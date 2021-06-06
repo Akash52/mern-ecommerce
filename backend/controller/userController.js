deb956dfcdc385b72294de54a701579d5894cd34
Akash52 <ac8572611@gmail.com>
2021-06-06 14:37:53 +0530

    User Authentication Endpoint

---
+++ backend/controller/userController.js
@@ -0,0 +1,27 @@
+import AsyncHandler from 'express-async-handler'
+import User from '../models/userModel.js'
+
+//@desc     Auth user & get Token
+//@route    POST/api/users/login
+//@acess    public
+
+const authUser = AsyncHandler(async (req, res) => {
+  const { email, password } = req.body
+
+  const user = await User.findOne({ email })
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: null,
+    })
+  } else {
+    res.status(401)
+    throw new Error('Invalid  email or Password')
+  }
+})
+
+export { authUser }
--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,10 @@ import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
-import {notFound,errorHandler} from './middleware/errorMiddleware.js'
-
-
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 //Connect Database
 connectDB()
@@ -14,16 +13,24 @@ connectDB()
 dotenv.config()
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('Api is runnig...')
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 //Custom Error Handle
-app.use(notFound);
-app.use(errorHandler);
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT,console.log(`Server Running in ${process.env.NODE_ENV} on ${PORT}`.yellow.underline))
+app.listen(
+  PORT,
+  console.log(
+    `Server Running in ${process.env.NODE_ENV} on ${PORT}`.yellow.underline
+  )
+)
